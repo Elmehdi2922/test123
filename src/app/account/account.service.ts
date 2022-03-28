040f3239f00c3486ae37d2aa3120df3858245590
ELMEHDI KHALKI <elmehdi.khalki@capgemini.com>
2022-03-28 15:24:36 +0000

    first commit

---
+++ src/app/account/account.service.ts
@@ -0,0 +1,79 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, of, ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { IAddress } from '../shared/models/address';
+import { IUser } from '../shared/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new ReplaySubject<IUser>(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  loadCurrentUser(token: string) {
+    if (token == null) {
+      this.currentUserSource.next(null);
+      return of(null);
+    }
+
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Bearer ${token}`);
+
+    return this.http.get(this.baseUrl + 'account', {headers}).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.id);
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  login(values: any) {
+    return this.http.get('https://jsonplaceholder.typicode.com/users?email=' + values.email).pipe(
+      map((user: IUser) => {
+        if (user) {
+          console.log(JSON.stringify(user));
+          localStorage.setItem('token', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  register(values: any) {
+    return this.http.post(this.baseUrl + 'https://jsonplaceholder.typicode.com/users?name=', values).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.id);
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  checkEmailExists(email: string) {
+    return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
+  }
+
+  getUserAddress() {
+    return this.http.get<IAddress>(this.baseUrl + 'account/address');
+  }
+
+  updateUserAddress(address: IAddress) {
+    return this.http.put<IAddress>(this.baseUrl + 'account/address', address);
+  }
+}
---
+++ src/app/shared/models/user.ts
@@ -0,0 +1,16 @@
+/*export interface IUser {
+    email: string;
+    displayName: string;
+    token: string;
+}*/
+
+export interface IUser {
+    id: string;
+    name: string;
+    username: string;
+    email: string;
+    address:any;
+    phone: string;
+    website: string;
+    company: any;
+}
